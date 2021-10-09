1476da651a5229b327f18b23faa2e0e20cec4864
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2021-10-09 20:27:02 +0800

    Update SaveAllCommand.java

--- src/main/java/baritone/command/defaults/SaveAllCommand.java
+++ src/main/java/baritone/command/defaults/SaveAllCommand.java
@@ -54,7 +54,7 @@ public class SaveAllCommand extends Command {
         return Arrays.asList(
                 "The saveall command saves Baritone's world cache.",
                 "",
-                "Usage:",
+                "使用方法:",
                 "> saveall"
         );
     }

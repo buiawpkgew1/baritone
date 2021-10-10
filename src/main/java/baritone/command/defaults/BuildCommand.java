da0291a0e9199bc7b42ee93314dcafa9b5b472c6
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2021-10-10 10:31:52 +0800

    Update BuildCommand.java

--- src/main/java/baritone/command/defaults/BuildCommand.java
+++ src/main/java/baritone/command/defaults/BuildCommand.java
@@ -58,9 +58,9 @@ public class BuildCommand extends Command {
         }
         boolean success = baritone.getBuilderProcess().build(file.getName(), file, buildOrigin);
         if (!success) {
-            throw new CommandInvalidStateException("Couldn't load the schematic. Make sure to use the FULL file name, including the extension (e.g. blah.schematic).");
+            throw new CommandInvalidStateException("无法加载原理图。请确保使用完整的文件名，包括扩展名（如：blah.schematic）.");
         }
-        logDirect(String.format("Successfully loaded schematic for building\nOrigin: %s", buildOrigin));
+        logDirect(String.format("成功加载building/Origin的原理图: %s", buildOrigin));
     }
 
     @Override
@@ -82,7 +82,7 @@ public class BuildCommand extends Command {
     @Override
     public List<String> getLongDesc() {
         return Arrays.asList(
-                "Build a schematic from a file.",
+                "从一个文件建立一个原理图.",
                 "",
                 "使用方法:",
                 "> build <filename> - Loads and builds '<filename>.schematic'",

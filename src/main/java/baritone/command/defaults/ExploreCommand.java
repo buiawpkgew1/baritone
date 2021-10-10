a7c97173c5dd6a227fd82694d3608fa8616b078d
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2021-10-10 10:31:33 +0800

    Update ExploreCommand.java

--- src/main/java/baritone/command/defaults/ExploreCommand.java
+++ src/main/java/baritone/command/defaults/ExploreCommand.java
@@ -45,7 +45,7 @@ public class ExploreCommand extends Command {
                 ? args.getDatatypePost(RelativeGoalXZ.INSTANCE, ctx.playerFeet())
                 : new GoalXZ(ctx.playerFeet());
         baritone.getExploreProcess().explore(goal.getX(), goal.getZ());
-        logDirect(String.format("Exploring from %s", goal.toString()));
+        logDirect(String.format("从%s探索", goal.toString()));
     }
 
     @Override
@@ -58,7 +58,7 @@ public class ExploreCommand extends Command {
 
     @Override
     public String getShortDesc() {
-        return "Explore things";
+        return "探索事物";
     }
 
     @Override
